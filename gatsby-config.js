fc87c694ca2953de160ac52dcd7bb854b0c04df4
eternalabu <abubakar.daud@mail.mcgill.ca>
2020-09-28 01:25:26 -0400

    small fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  pathPrefix: "/abubakardaud.github.io",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Abu`,
+    description: `Personal website`,
+    author: `Abu`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
